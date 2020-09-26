3d7c555b9e9f91b693e0b4e28e518f6a3d9695e7
KID <50666207+KID421@users.noreply.github.com>
2020-09-26 19:08:45 +0800

    AR

---
+++ AR/Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public VirtualButtonBehaviour vbb;
+    public Material model;
+
+    private void Start()
+    {
+        vbb.RegisterOnButtonPressed(Pressed());
+    }
+
+    private System.Action<VirtualButtonBehaviour> Pressed()
+    {
+        model.color += new Color(0.1f, 0, 0);
+        return null;
+    }
+}
